020f8aad643f54f0a155ec15ef996a27955050fd
tforbus <tristinforbus@gmail.com>
2013-11-22 20:56:36 -0600

    working on styling

--- public/javascripts/drums/directives/drumTick.js
+++ public/javascripts/drums/directives/drumTick.js
@@ -3,10 +3,17 @@ angular.module('drums.directives')
 /**
  * Toggles the color and state of the drum tick.
  */
-.directive('drumTick', function () {
+.directive('currentTick', function () {
   return {
     restrict: 'A',
+    require: 'ngModel',
+    scope: {model: '=ngModel'},
     link: function (scope, elem, attrs) {
+      var index = attrs.tdIndex;
+      if (scope.model.currentTick === index) {
+        elem.addClass('current-tick');
+        elem.siblings().removeClass('current-tick');
+      }
     }
   };
 });
